ec6668dc38cfa6f133e7a786375a2f0f8374a0ed
Efe Karaman <efekaraman@hotmail.com>
2024-01-26 15:22:05 +0300

    Created the generic service interface

---
+++ VakaAPI/Services/IGenericService.cs
@@ -0,0 +1,11 @@
+namespace VakaAPI.Services
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
